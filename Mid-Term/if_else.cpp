54a336be77a5b63a084caf3c645175a002208a0d
Mrx™ <itzrakib34@gmail.com>
2024-05-10 16:19:15 +0700

    #7
    #7

---
+++ Mid-Term/if_else.cpp
@@ -0,0 +1,27 @@
+//Name:MRXVAU
+//ID : 24-56434-1
+//SERIAL : 28
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0) {
+        cout << "The number is positive." << endl;
+    } 
+    else if (num < 0) {
+        cout << "The number is negative." << endl;
+    } 
+    else {
+        cout << "The number is zero." << endl;
+    }
+
+    return 0;
+}
